669c389887a4210c809ad081262810d56c644486
Matthew Jensen <mrjnox@gmail.com>
2019-09-29 12:55:18 -0500

    merge in steps from legacy "pacman.sh"

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+packages='gvim bspwm sxhkd rofi xterm glances htop rsync ttf-hack compton pulseaudio pulseaudio-alsa also-utils maim xclip mpv feh youtube-dl tmux imagemagick base-devel neofetch libusb i3lock bind geoip nmap whois conky git xorg xorg-xinit arch-install-scripts w3m openssh mediainfo ed zathura zathura-pdf-mupdf ranger mlocate lshw qutebrowser'
+
 echo "Installing Arch Linux"
+
+echo "Installing extra packages"
+pacman -S ${packages}
+
+echo "Cloning yay"
+git clone https://aur.archlinux.org/yay.git
